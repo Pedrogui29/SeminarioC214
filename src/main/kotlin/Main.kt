9062b4b804351e16aae7d37ad6ce1a9064a29a34
Pedro Guilherme <pedropgfo@gmail.com>
2024-10-23 16:54:29 -0300

    Implementando classe main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,44 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val library = Library()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Adicionando alguns livros ao sistema
+    val book1 = Book("1984", "George Orwell")
+    val book2 = Book("Brave New World", "Aldous Huxley")
+    val book3 = Book("Fahrenheit 451", "Ray Bradbury")
+
+    library.addBook(book1)
+    library.addBook(book2)
+    library.addBook(book3)
+
+    println("Livros disponíveis na biblioteca:")
+    library.listAvailableBooks().forEach { println(it.title) }
+
+    // Emprestando o primeiro livro
+    println("\nEmprestando '1984'...")
+    if (library.borrowBook(book1)) {
+        println("'1984' foi emprestado com sucesso!")
+    } else {
+        println("'1984' não está disponível para empréstimo.")
+    }
+
+    // Tentando emprestar o mesmo livro novamente
+    println("\nTentando emprestar '1984' novamente...")
+    if (library.borrowBook(book1)) {
+        println("'1984' foi emprestado com sucesso!")
+    } else {
+        println("'1984' não está disponível para empréstimo.")
+    }
+
+    // Listando livros disponíveis após o empréstimo
+    println("\nLivros disponíveis após o empréstimo:")
+    library.listAvailableBooks().forEach { println(it.title) }
+
+    // Devolvendo o livro emprestado
+    println("\nDevolvendo '1984'...")
+    library.returnBook(book1)
+    println("'1984' foi devolvido.")
+
+    // Listando livros disponíveis após a devolução
+    println("\nLivros disponíveis após a devolução:")
+    library.listAvailableBooks().forEach { println(it.title) }
 }
