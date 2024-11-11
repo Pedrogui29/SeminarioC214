4312762750bae938dc897c5f852ac0e6d85f9baa
natachacgr <natacha.calixto@ges.inatel.br>
2024-11-11 20:17:17 -0300

    teste listagem de livros disponiveis

--- src/test/kotlin/LibraryTest.kt
+++ src/test/kotlin/LibraryTest.kt
@@ -1,5 +1,6 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldNotContain
 import io.kotest.matchers.shouldBe
 
 class LibraryTest : StringSpec({
@@ -42,5 +43,17 @@ class LibraryTest : StringSpec({
         library.returnBook(book)
 
         book.isAvailable shouldBe true
+    }
+
+    "should list only available books" {
+        val library = Library()
+        val book1 = Book("The Catcher in the Rye", "J.D. Salinger", isAvailable = true)
+        val book2 = Book("Brave New World", "Aldous Huxley", isAvailable = false)
+
+        library.addBook(book1)
+        library.addBook(book2)
+
+        library.listAvailableBooks() shouldContain book1
+        library.listAvailableBooks() shouldNotContain book2
     }
 })
